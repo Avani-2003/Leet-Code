651487af91cf303326819f91f03a13b6a7dbab1e
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-04-06 13:00:58 +0530

    Time: 10 ms (99.46%) | Memory: 11.2 MB (94.79%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,38 +1,30 @@
-
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        // Initialize variables
-        int openParenthesesCount = 0;
-        vector<char> arr(s.begin(), s.end()); // Convert string to character array
-
-        // First pass: mark excess closing parentheses with '*'
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] == '(')
-                openParenthesesCount++;
-            else if (arr[i] == ')') {
-                if (openParenthesesCount == 0)
-                    arr[i] = '*'; // Mark excess closing parentheses
-                else
-                    openParenthesesCount--;
+        int count=0;
+        vector<int> arr;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='('){
+                arr.push_back(i);
+                count++;
+            } 
+            else if(s[i]==')'&& count>0) {
+                arr.pop_back();
+                count--;
             }
-        }
-
-        // Second pass: mark excess opening parentheses from the end
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            if (openParenthesesCount > 0 && arr[i] == '(') {
-                arr[i] = '*'; // Mark excess opening parentheses
-                openParenthesesCount--;
+            else if(s[i]==')' && count==0){
+                s.erase(s.begin()+i);
+                i--;
             }
+        } 
+       // cout<<"C= "<<count<<endl;
+        int i=0;
+        while(arr.size()>0)
+        {
+            s.erase(s.begin()+arr.back());
+            arr.pop_back();
         }
-        
-        // Filter out marked characters and store the result in a new string
-        string result = "";
-        for (char c : arr) {
-            if (c != '*')
-                result += c;
-        }
-
-        return result;
+        return s;       
     }
 };
