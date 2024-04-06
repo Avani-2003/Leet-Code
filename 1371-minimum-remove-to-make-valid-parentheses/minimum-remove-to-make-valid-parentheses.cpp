36e0ed547e244c667716fab4c27f4902df50d2be
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-04-06 13:00:25 +0530

    Time: 10 ms (99.46%) | Memory: 12.7 MB (60.56%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,30 +1,38 @@
+
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        int count=0;
-        vector<int> arr;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]=='('){
-                arr.push_back(i);
-                count++;
-            } 
-            else if(s[i]==')'&& count>0) {
-                arr.pop_back();
-                count--;
+        // Initialize variables
+        int openParenthesesCount = 0;
+        vector<char> arr(s.begin(), s.end()); // Convert string to character array
+
+        // First pass: mark excess closing parentheses with '*'
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == '(')
+                openParenthesesCount++;
+            else if (arr[i] == ')') {
+                if (openParenthesesCount == 0)
+                    arr[i] = '*'; // Mark excess closing parentheses
+                else
+                    openParenthesesCount--;
             }
-            else if(s[i]==')' && count==0){
-                s.erase(s.begin()+i);
-                i--;
-            }
-        } 
-       // cout<<"C= "<<count<<endl;
-        int i=0;
-        while(arr.size()>0)
-        {
-            s.erase(s.begin()+arr.back());
-            arr.pop_back();
         }
-        return s;       
+
+        // Second pass: mark excess opening parentheses from the end
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            if (openParenthesesCount > 0 && arr[i] == '(') {
+                arr[i] = '*'; // Mark excess opening parentheses
+                openParenthesesCount--;
+            }
+        }
+        
+        // Filter out marked characters and store the result in a new string
+        string result = "";
+        for (char c : arr) {
+            if (c != '*')
+                result += c;
+        }
+
+        return result;
     }
 };
