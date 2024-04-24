cee7ffb14ab787d802dba16af457cf05cf5cb6ed
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-04-24 20:21:05 +0530

    Time: 3 ms (22.77%) | Memory: 7 MB (81.41%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -4,13 +4,15 @@ public:
         if(n==1 || n==2) return 1;
         else if(n==0) return 0;
         else{
-            vector <int> arr;
-            arr.push_back(1);
-            arr.push_back(1);
-            arr.push_back(2);
-            for(int i=3;i<n;i++)
-            arr.push_back(arr[i-1]+arr[i-3]+arr[i-2]);
-            return arr[n-1];
+            int d,a=0,b=1,c=1;
+            for(int i=3;i<=n;i++)
+            {
+                d=c;
+                c=a+b+c;
+                a=b;
+                b=d;
+            }
+            return c;
         }
         return 0;
     }
