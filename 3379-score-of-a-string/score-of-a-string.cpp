8833e482a4f0e43442d6c8c2f1a9c662f8d2a331
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-06-01 11:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (42.17%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-     int count=0,n=s.size();
-     for(int i=0;i<n-1;i++)
-     {
-        count=count+abs(int(s[i])-int(s[i+1]));
-     }
-     return count;
+        int n=s.length(),count=0;
+        for(int i=0;i<n-1;i++)
+        {
+            count = count + abs(s[i]-s[i+1]);
+        }
+        return count;
     }
 };
