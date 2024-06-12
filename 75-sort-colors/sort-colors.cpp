0d7927d87a0c27ce9076bed2d0dbf22216d22110
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-06-12 12:26:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (34.14%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int start = 0 , end = nums.size();
-        sort(nums.begin(),nums.end());
+        int low = 0 , end = nums.size()-1 , mid = 0 ;
+        while(mid <= end)
+        {
+            if(nums[mid] == 0)
+            {
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else {
+                if(nums[mid] == 1)
+                mid++;
+                else{
+                    swap(nums[mid],nums[end]);
+                    end--;
+                }
+            }
+        }
     }
 };
