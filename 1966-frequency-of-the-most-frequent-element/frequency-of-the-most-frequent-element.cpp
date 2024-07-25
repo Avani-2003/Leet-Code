781bff31446d368a0f87d5f20e103d90fc21e400
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-07-25 19:24:18 +0530

    Time: 145 ms (86.78%) | Memory: 105.9 MB (17.43%) - LeetSync

---
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        long long int freq = 1,l = 0,r = 0, n = nums.size(),total = 0;
+        for(r=0;r<n;r++)
+        {
+            total = total + nums[r];
+            if((nums[r]*(r-l+1))>total+k)
+            {
+                total-=nums[l];
+                l++;
+            }
+            freq = max(freq,r-l+1);
+        }
+        return freq;
+    }
+};
