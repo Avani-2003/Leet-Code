d6b090180697da14033727327e0ef87e4d6cf972
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-07-30 14:30:03 +0530

    Time: 144 ms (37.18%) | Memory: 75 MB (13.70%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,23 +10,49 @@
  */
 class Solution {
 public:
+    ListNode* middle(ListNode* head)
+    {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* slow = head, *fast = head->next;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* left, ListNode* right)
+    {
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        while(left && right)
+        {
+            if(left->val<=right->val)
+            {
+                temp->next = left;
+                left = left->next;
+            }
+            else{
+                temp->next = right;
+                right = right->next;
+            }
+            temp = temp->next;
+        }
+        if(left!=NULL)
+            temp->next = left;
+        else temp->next = right;
+        return dummy->next;
+    }
+
     ListNode* sortList(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
-        vector<int> arr;
-        while(head)
-        {
-            arr.push_back(head->val);
-            head = head->next;
-        }
-        sort(arr.begin(),arr.end());
-        ListNode* ans = new ListNode(0);
-        ListNode* temp = ans;
-        for(int i=0;i<arr.size();i++)
-        {
-            ListNode* hold = new ListNode(arr[i]);
-            temp->next = hold;
-            temp = hold;
-        }
-        return ans->next;
+        ListNode* left = head;
+        ListNode* mid = middle(head);
+        ListNode* right = mid->next;
+        mid->next = NULL;
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left, right);
     }
 };
