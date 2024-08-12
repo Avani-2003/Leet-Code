1c2a844a4b972529c01f4fc4c1b560c1abfd3d49
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-08-12 18:40:43 +0530

    Time: 1059 ms (24.61%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,1 +1,2 @@
-SELECT Emp.name , B.bonus FROM Employee AS Emp LEFT JOIN Bonus AS B  ON Emp.empId=B.empId WHERE B.bonus<1000 OR BONUS IS NULL;
+select emp.name , B.bonus from Employee emp left join Bonus B
+on emp.empId=B.empId where B.bonus<1000 or B.bonus is null;
